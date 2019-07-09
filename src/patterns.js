7330191a3cebcf8dae10ea7fc598ac38967863e8
Allen Zhao <995029442@qq.com>
2019-07-09 14:54:23 +0800

    add Observer

--- src/patterns.js
+++ src/patterns.js
@@ -1,3 +1,76 @@
+/**
+ * 观察者模式
+ */
+class ObserverList {
+  _observerList = []
+
+  add(obj) {
+    // 返回 _observerList 的新长度
+    return this._observerList.push(obj)
+  }
+
+  empty() {
+    this._observerList = []
+  }
+
+  count() {
+    return this._observerList.length
+  }
+
+  get(index) {
+    if (index > -1 && index < this._observerList.length) {
+      return this._observerList[index]
+    }
+  }
+
+  insert(obj, index) {
+    let point = -1
+    if (index > -1 && index <= this._observerList.length) {
+      this._observerList.splice(index, 0, obj)
+      point = index
+    }
+    return index
+  }
+
+  indexOf(obj, startIndex = 0) {
+    return this._observerList.indexOf(obj, startIndex)
+  }
+
+  removeAt(index) {
+    if (index > -1 && index < this._observerList.length) {
+      this._observerList.splice(index, 1)
+    }
+  }
+}
+
+class Subject {
+  _observers = new ObserverList()
+
+  addObserver(observer) {
+    this._observers.add(observer)
+  }
+
+  removeObserver(observer) {
+    this._observers.removeAt(this._observers.indexOf(observer))
+  }
+
+  notify(context) {
+    let l = this._observers.count()
+    while (l--) {
+      this._observers.get(l).update(context)
+    }
+  }
+}
+
+class Observer {
+  constructor(update) {
+    this.update = update
+  }
+}
+
+/**
+ * 发布订阅模式
+ */
 class Pubsub {
   _topics = {}
   _subUid = -1
@@ -43,5 +116,7 @@ class Pubsub {
 }
 
 export {
-  Pubsub
+  Pubsub,
+  Subject,
+  Observer,
 }
